4b7fe089c4f77d5b1f2411f651d5c4a8580a9fc5
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2026-05-02 22:52:50 +0500

    Implement order event publising

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderTrackCommandHandler.java
@@ -0,0 +1,19 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+@Slf4j
+@Validated
+@Component
+@RequiredArgsConstructor
+public class OrderTrackCommandHandler {
+
+    public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery) {
+        return null;
+    }
+}
