d371807ac96d82bb7ca4dae62bd0b386e0e6dd06
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2026-05-03 23:14:35 +0500

    Add mockito and spring testing for order domain service

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/track/TrackOrderResponse.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/track/TrackOrderResponse.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -16,5 +17,5 @@ public class TrackOrderResponse {
     private final UUID orderTrackingId;
     @NotNull
     private final OrderStatus orderStatus;
-    private final String message;
+    private final List<String> failureMessages;
 }
